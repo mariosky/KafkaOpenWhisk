ce511f074f8b2ca9db5b3a20dbcf833561d2a234
Mario Garcia Valdez <mariosky@gmail.com>
2018-01-29 14:19:16 -0800

    kafka experiment

--- kafka_controller.py
+++ kafka_controller.py
@@ -22,8 +22,6 @@ with open(secret_credentials) as json_data:
     secret_opts = json.load(json_data)
 
 
-from collections import deque
-queue = deque()
 
 def cxBestFromEach(pop1, pop2, key = lambda p: p['fitness']['score']):
     # small is better
@@ -59,7 +57,8 @@ def on_delivery( err, msg):
         print(err)
             # We could retry sending the message
     else:
-        print('offset: {0}'.format(msg.offset())),
+        pass
+        #print('offset: {0}'.format(msg.offset())),
 
 
 
@@ -75,7 +74,7 @@ def experiment(env):
     # Counter for experiments, some times we receive from earlier problems
     # Set to zero if not exists
     problem_id = env["problem"]["problem_id"]
-    queue.clear()
+    queue = []
 
     running = True
 
@@ -88,32 +87,37 @@ def experiment(env):
             # Increment counter only if message from this experiment
             if problem_id == pop["problem"]["problem_id"]:
                 count+=1
-                print count, pop["problem"]["problem_id"]
+                #print count, pop["problem"]["problem_id"]
 
                 # Get inline
-                queue.appendleft(pop)
+                queue.append(pop)
 
                 #TO DO: CROSSOVER POPULATION HERE
                 # If there is another pop in the queue
                 if len(queue) > 1:
                     # get other
-                    other = queue.pop()
+                    #other = queue.pop()
+                    # random from the last 10
+                    # percentege of length
+                    other = random.choice(queue)
                     #crossover
 
                     #cxOnePoint(pop['population'], other['population'])
                     pop['population'] = cxBestFromEach(pop['population'], other['population'])
-                    queue.appendleft(other)
-
+                    #queue.appendleft(other)
+                    #queue.append(other)
+                    print '.',
 
 
                 # Only return if we are in the same experiment
                 producer.produce('populations-topic', json.dumps(pop ), 'key', -1, on_delivery)
-            else:
-                print "old", pop["problem"]["problem_id"]
+                #Log any way, there is no problem if we evaluate more
+                if (REDIS_LOG):
+                    redis_log.log_to_redis_coco(json.loads(msg.value().decode('utf-8')))
+                else:
+                    print "old", pop["problem"]["problem_id"]
 
-            #Log any way, there is no problem if we evaluate more
-            if (REDIS_LOG):
-                redis_log.log_to_redis_coco(json.loads(msg.value().decode('utf-8')))
+
 
         elif msg.error().code() != confluent_kafka.KafkaError._PARTITION_EOF:
             print(msg.error())
--- kafka_experiment.py
+++ kafka_experiment.py
@@ -7,18 +7,18 @@ import process_logs
 
 
 conf = {
-    2: { 'NGEN':100, 'POP_SIZE': 200, 'MAX_ITERATIONS':10, 'MESSAGES_HUB_PSO':0, 'MESSAGES_HUB_GA':8 },
+    2: { 'NGEN':50, 'POP_SIZE': 100, 'MAX_ITERATIONS':20, 'MESSAGES_HUB_PSO':0, 'MESSAGES_HUB_GA':2 },
     3: { 'NGEN':50, 'POP_SIZE': 100, 'MAX_ITERATIONS':30, 'MESSAGES_HUB_PSO':0, 'MESSAGES_HUB_GA':2 },
     5: { 'NGEN':50, 'POP_SIZE': 100, 'MAX_ITERATIONS':25, 'MESSAGES_HUB_PSO':0, 'MESSAGES_HUB_GA':4 },
     10:{ 'NGEN':50, 'POP_SIZE': 200, 'MAX_ITERATIONS':25, 'MESSAGES_HUB_PSO':0, 'MESSAGES_HUB_GA':4 },
     20:{ 'NGEN':50, 'POP_SIZE': 200, 'MAX_ITERATIONS':25, 'MESSAGES_HUB_PSO':0, 'MESSAGES_HUB_GA':8 },
-    40:{ 'NGEN':50, 'POP_SIZE': 200, 'MAX_ITERATIONS':25, 'MESSAGES_HUB_PSO':0, 'MESSAGES_HUB_GA':8 },
+    40:{ 'NGEN':50, 'POP_SIZE': 200, 'MAX_ITERATIONS':25, 'MESSAGES_HUB_PSO':0, 'MESSAGES_HUB_GA':16 },
 
     'EXPERIMENT_ID' : int(time.time()),
-                               #For paper:
+     #For paper:
     'FUNCTIONS' : (3,),
     'DIMENSIONS' : (2,),       #(2,3,5,10,20)
-    'INSTANCES' : range(1, 3)  # range(1,6)+range(41, 51)
+    'INSTANCES' : range(1,3)    #range(1,6)+range(41, 51)
 
 }
 #Example from EvoSpace
@@ -29,13 +29,13 @@ conf = {
 
 # 40: 4,000,000
 
-#conf = {
-#    2: {'EVOSPACE_SIZE':250, 'NGEN':50, 'SAMPLE_SIZE': 100, 'MAX_SAMPLES':20, 'PSO':2, 'GA':0 },
-#    3: {'EVOSPACE_SIZE':250, 'NGEN':50, 'SAMPLE_SIZE': 100, 'MAX_SAMPLES':30, 'PSO':2, 'GA':0 },
-#    5: {'EVOSPACE_SIZE':500, 'NGEN':50, 'SAMPLE_SIZE': 100, 'MAX_SAMPLES':25, 'PSO':2, 'GA':2 },
-#    10:{'EVOSPACE_SIZE':1000,'NGEN':50, 'SAMPLE_SIZE': 200, 'MAX_SAMPLES':25, 'PSO':2, 'GA':2 },
-#    20:{'EVOSPACE_SIZE':2000,'NGEN':50, 'SAMPLE_SIZE': 200, 'MAX_SAMPLES':25, 'PSO':4, 'GA':4 },
-#    40:{'EVOSPACE_SIZE':4000,'NGEN':50, 'SAMPLE_SIZE': 200, 'MAX_SAMPLES':25, 'PSO':8, 'GA':8 },
+#es_conf = {
+#    2: {'EVOSPACE_SIZE':250, 'NGEN':50, 'SAMPLE_SIZE': 100, 'MAX_SAMPLES':20, 'PSO':0, 'GA':2 },
+#    3: {'EVOSPACE_SIZE':250, 'NGEN':50, 'SAMPLE_SIZE': 100, 'MAX_SAMPLES':30, 'PSO':0, 'GA':2 },
+#    5: {'EVOSPACE_SIZE':500, 'NGEN':50, 'SAMPLE_SIZE': 100, 'MAX_SAMPLES':25, 'PSO':0, 'GA':4 },
+#    10:{'EVOSPACE_SIZE':1000,'NGEN':50, 'SAMPLE_SIZE': 200, 'MAX_SAMPLES':25, 'PSO':0, 'GA':4 },
+#    20:{'EVOSPACE_SIZE':2000,'NGEN':50, 'SAMPLE_SIZE': 200, 'MAX_SAMPLES':25, 'PSO':0, 'GA':8 },
+#    40:{'EVOSPACE_SIZE':4000,'NGEN':50, 'SAMPLE_SIZE': 200, 'MAX_SAMPLES':25, 'PSO':0, 'GA':16 },
 #}
 
 
@@ -72,7 +72,7 @@ def experiment(conf):
                  "id": "1",
                  "algorithm": {"crossover": {"type": "cxTwoPoint", "CXPB": [0, 0.2]}, "name": "GA",
                                "mutation": {"MUTPB": 0.5, "indpb": 0.05, "sigma": 0.5, "type": "mutGaussian", "mu": 0},
-                               "selection": {"type": "tools.selTournament", "tournsize": 8},
+                               "selection": {"type": "tools.selTournament", "tournsize": 2},
                                "iterations": conf[dim]['NGEN']},
                  "experiment":
                      {"owner": "mariosky", "type": "benchmark", "experiment_id": conf['EXPERIMENT_ID']}}
